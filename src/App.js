41a017a2705716add30b78e0a4a8debf969aae82
cheerfulleg <61907072+cheerfulleg@users.noreply.github.com>
2020-10-17 15:54:47 +0300

    implemented firebase utils, including ability to store authenticated users in firestore

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Header from "./components/header/header.component";
 import HomePage from './pages/homepages/homepage.component';
 import ShopPage from "./pages/shop/shop.component";
 import SignInAndSignUp from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
-import {auth} from "./firebase/firebase.utlis";
+import {auth, createUserProfileDocument} from "./firebase/firebase.utlis";
 
 class App extends React.Component {
     constructor(props) {
@@ -21,10 +21,25 @@ class App extends React.Component {
     unsubscribeFromAuth = null;
 
     componentDidMount() {
-        this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-            this.setState({currentUser: user});
+        this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+            if(userAuth) {
+                const userRef = await createUserProfileDocument(userAuth);
 
-            console.log(user)
+                userRef.onSnapshot(snapshot => {
+                    this.setState({
+                        currentUser: {
+                            id: snapshot.id,
+                            ...snapshot.data()
+                        }
+                    });
+                    console.log(this.state)
+                });
+            }
+            else {
+                this.setState({
+                    currentUser: userAuth
+                })
+            }
         })
     }
 
--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.js
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.js
@@ -1,11 +1,13 @@
 import React from "react";
 import SingIn from "../../components/sing-in/sing-in.component";
+import SignUp from "../../components/sign-up/sign-up.component";
 
 import './sign-in-and-sign-up.styles.scss';
 
 const SignInAndSignUp = () => (
     <div className="sign-in-and-sign-up">
         <SingIn />
+        <SignUp />
     </div>
 )
 
