39810bb809b6db6416b83c70a527ef3f1a234aaa
cheerfulleg <61907072+cheerfulleg@users.noreply.github.com>
2020-10-24 01:22:08 +0300

    updated styles for mobile view

--- src/components/sign-up/sign-up.component.js
+++ src/components/sign-up/sign-up.component.js
@@ -8,8 +8,8 @@ import { auth, createUserProfileDocument } from '../../firebase/firebase.utils';
 import './sign-up.styles.scss';
 
 class SignUp extends React.Component {
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
 
         this.state = {
             displayName: '',
